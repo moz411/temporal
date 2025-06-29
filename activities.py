33fe92764e00b5bfbce16931f0b21ac0f56f58f6
Thomas Dupouy <thomas.dupouy@moz42.net>
2025-06-29 20:34:22 +0000

    InterpreterDiscoveryRequiredError

--- activities.py
+++ activities.py
@@ -1,35 +1,49 @@
 import asyncio
 from temporalio import activity
-from ansible.executor.task_executor import TaskExecutor
+from ansible.playbook.play import Play
+from ansible.playbook.block import Block
 from ansible.playbook.task import Task
+from ansible.executor.task_executor import TaskExecutor
 from ansible.parsing.dataloader import DataLoader
 from ansible.inventory.manager import InventoryManager
 from ansible.vars.manager import VariableManager
 from ansible.playbook.play_context import PlayContext
 from ansible.inventory.host import Host
-from ansible.plugins.connection.local import Connection
+from ansible.plugins import loader as first_loader
 
-def parse_task_dict(task_dict: dict) -> Task:
-    loader = DataLoader()
-    inventory = InventoryManager(loader=loader, sources=["localhost,"])
-    variable_manager = VariableManager(loader=loader, inventory=inventory)
-
-    task_obj = Task.load(task_dict, variable_manager=variable_manager, loader=loader)
-    return task_obj
 
 @activity.defn
 async def run_ansible_task(params) -> list:
     result = []
+    play_context = PlayContext()
+    play_context.connection = "local"
     host = Host(params.get('host', 'localhost'))
-    task = parse_task_dict(params.get('task', {}))
+    task_dict = params.get('task', {})
+    loader = DataLoader()
+    inventory = InventoryManager(loader=loader, sources=["localhost ansible_connection=local ansible_python_intepreter='/usr/bin/python3,"])
+    variable_manager = VariableManager(loader=loader, inventory=inventory)
+    task = Task.load(task_dict, variable_manager=variable_manager, loader=loader)
     task_vars = {}
-    play_context =  PlayContext()
     new_stdin =  {}
-    loader =  DataLoader()
-    shared_loader_obj =  loader
+    shared_loader_obj =  first_loader
     final_q =  {}
 
-    # res = AdHocCLI(args=cmd).run()
+    play = Play().load(
+        {
+            "name": "Temporal Play",
+            "hosts": "localhost",
+            "gather_facts": False,
+            "tasks": [task_dict],  # nécessaire pour initialiser correctement les attrs internes
+        },
+        variable_manager=variable_manager,
+        loader=loader,
+    )
+
+    block = play.compile()[0]  # → premier bloc (Block) compilé
+    # Rattacher proprement les références
+    task._parent = block
+    block._play = play  # ← ceci évite ton erreur
+    
     executor_result = TaskExecutor(
             host,
             task,
